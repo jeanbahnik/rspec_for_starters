5406e1155f9197c501c41d567ad98db659f93c8e
Jean Bahnik <jeanbahnik@gmail.com>
2013-09-05 22:18:45 -0500

    resetting seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
-User.create([{ first_name: 'Jeff' }, { last_name: 'Cohen' }, { email: 'jeff@starterleague.com' }])
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
