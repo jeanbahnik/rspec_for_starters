3db9ebeb5dde41e2eef159dc189cd6736ee8466d
Mike Ebert <mike@mikeebert.net>
2013-09-05 21:33:39 -0500

    two passing and one failing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
   def show
+    id = params[:id]
+    @user = User.find_by_id(id)
   end
 
+  def update
+    @user = User.find_by_id(params[:id])
+
+    if @user.update_attributes(params[:user])
+      redirect_to(user_path)
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,31 @@
 require 'spec_helper'
 
 describe UsersController do
+  before :each do
+    @user = User.new(:first_name => 'Jeff', :last_name => 'Cohen')
+    User.stub(:find_by_id).and_return(@user)
+  end
+
   describe "user#show" do
     it "returns a 200 when calling the show method" do
       get :show
       response.status.should be(200)
+    end
+
+    it "assigns the user" do
+      get :show, :id => @user.id
+
+      assigns[:user].should == @user
+    end
+  end
+
+  describe '#update' do
+    it "redirects to the user show on a successful udpate" do
+      @user.should_receive(:update_attributes).and_return(true)
+
+      put :update, :user => {:first_name => 'El Jefe', :last_name => "teacher man"}
+
+      response.should redirect_to(user_path)
     end
   end
 end
